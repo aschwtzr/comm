ff5041ccfb4c4bb53d0c54b464d0c870fee10b2f
Abosh Upadhyaya <aboshu@gmail.com>
2022-04-27 19:01:54 -0700

    [native] Add active state to `ColorSelector`
    Summary:
    Part of ongoing redesign of `ColorPicker` component on `native` to match the `ColorSelector` component on `web`. Depends on D3798.
    
    Adds active state to `ColorSelector` by adding styling and a checkmark to the `ColorSelectorButton` that is currently selected as the thread color.
    
    Will add `onPress` handling and updating the thread color in a later diff, as outlined in the plan on this Linear issue.
    
    Test Plan: Tested on iOS 15.4 and works as expected:
    
    Reviewers: atul
    
    Subscribers: ashoat, palys-swm, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D3868

--- native/components/color-selector-button.react.js
+++ native/components/color-selector-button.react.js
@@ -1,25 +1,42 @@
 // @flow
 
 import * as React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { TouchableOpacity, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import tinycolor from 'tinycolor2';
 
 type ColorSelectorButtonProps = {
-  color: string,
+  +color: string,
+  +currentColor: string,
 };
 
 function ColorSelectorButton(props: ColorSelectorButtonProps): React.Node {
-  const { color } = props;
-  const buttonStyle = React.useMemo(
-    () => [styles.button, { backgroundColor: color }],
-    [color],
+  const { color, currentColor } = props;
+
+  const colorSplotchStyle = React.useMemo(() => {
+    return [styles.button, { backgroundColor: `#${color}` }];
+  }, [color]);
+
+  const isSelected = tinycolor.equals(currentColor, color);
+  const selectedButtonIndicator = React.useMemo(() => {
+    if (isSelected) {
+      return <Icon name="ios-checkmark" size={36} color="white" />;
+    }
+  }, [isSelected]);
+
+  return (
+    <TouchableOpacity style={colorSplotchStyle}>
+      {selectedButtonIndicator}
+    </TouchableOpacity>
   );
-  return <View style={buttonStyle} />;
 }
 
 const styles = StyleSheet.create({
   button: {
+    alignItems: 'center',
     borderRadius: 20,
     height: 40,
+    justifyContent: 'center',
     margin: 15,
     width: 40,
   },
