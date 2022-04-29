a522ad7e6ba4b79ed02f6e1dfd7ad33dbdf5e999
Jonathan Ringer <jonringer117@gmail.com>
2022-04-28 21:20:25 -0700

    Update dev shell
    Summary:
    A lot has happened since the first initial yarn dev diff
    This is to update the dependencies as they have evolved since
    then
    
    Depends on D3865
    
    Test Plan:
    Run `nix develop` to create shell.
    Run `yarn cleaninstall` to build all yarn projects
    
    Subscribers: ashoat, palys-swm, Adrian, atul, karol-bisztyga, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3874

--- nix/dev-shell.nix
+++ nix/dev-shell.nix
@@ -2,9 +2,16 @@
 , stdenv
 , lib
 , arcanist
+, cargo
+, cmake
 , darwin
+, grpc
 , nodejs-16_x
-, protobuf3_15
+, protobuf_3_15_cmake
+, python2
+, python3
+, sqlite
+, watchman
 , yarn
 }:
 
@@ -12,17 +19,33 @@ mkShell {
 
   # programs which are meant to be executed should go here
   nativeBuildInputs = [
+    # generic development
     arcanist
+
+    # node development
     nodejs-16_x
-    protobuf3_15
+    sqlite
     yarn
+    watchman # react native
+    python2
+    python3
+
+    # native dependencies
+    # C/CXX toolchains are already brought in with mkShell
+    cargo # includes rustc
+    cmake
+    protobuf_3_15_cmake
+    grpc
+
   ];
 
   # include any libraries buildInputs
   buildInputs = [
-    protobuf3_15 # exposes both a library and a command, thus should appear in both inputs
+    protobuf_3_15_cmake # exposes both a library and a command, thus should appear in both inputs
+    sqlite
   ] ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
     CoreFoundation
+    CoreServices
     Security
   ]);
 
