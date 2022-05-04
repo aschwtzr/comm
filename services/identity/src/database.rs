7466d783880d03ce989ef5f136cc9d99a55a3edf
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2022-05-04 16:31:36 -0400

    [Identity] Implement database client method to get pake registration
    Summary:
    To log in a user with PAKE, the Identity service will need to fetch the user's PAKE registration data from DynamoDB. This diff introduces a method to do that. The user ID is the table's primary key, so we use that to call `get_item`. Then we deserialize the bytes stored in DDB to get a ServerRegistration object. If the client is unable to get a ServerRegistration object, it logs the error and returns None.
    
    Depends on D3920
    
    Test Plan: Tested all failure cases and the success case by having the client get an item from a sample DDB table
    
    Reviewers: jimpo, palys-swm, karol-bisztyga
    
    Subscribers: ashoat, Adrian, yayabosh, atul
    
    Differential Revision: https://phabricator.ashoat.com/D3921

--- services/identity/src/database.rs
+++ services/identity/src/database.rs
@@ -1,5 +1,14 @@
-use rusoto_core::Region;
-use rusoto_dynamodb::DynamoDbClient;
+use std::collections::HashMap;
+
+use opaque_ke::{errors::ProtocolError, ServerRegistration};
+use rusoto_core::{Region, RusotoError};
+use rusoto_dynamodb::{
+  AttributeValue, DynamoDb, DynamoDbClient, GetItemError, GetItemInput,
+  GetItemOutput,
+};
+use tracing::{error, info};
+
+use crate::opaque::Cipher;
 
 pub struct DatabaseClient {
   client: DynamoDbClient,
@@ -11,4 +20,77 @@ impl DatabaseClient {
       client: DynamoDbClient::new(region),
     }
   }
+
+  pub async fn get_pake_registration(
+    &self,
+    user_id: String,
+  ) -> Result<Option<ServerRegistration<Cipher>>, Error> {
+    let partition_key = HashMap::from([(
+      "userID".to_string(),
+      AttributeValue {
+        s: Some(user_id.clone()),
+        ..Default::default()
+      },
+    )]);
+    let get_item_input = GetItemInput {
+      table_name: "identity-pake-registration".to_string(),
+      key: partition_key,
+      consistent_read: Some(true),
+      ..GetItemInput::default()
+    };
+    let get_item_result = self.client.get_item(get_item_input).await;
+    match get_item_result {
+      Ok(GetItemOutput {
+        item: Some(item), ..
+      }) => {
+        if let Some(AttributeValue {
+          b: Some(server_registration_bytes),
+          ..
+        }) = item.get("pakeRegistrationData")
+        {
+          match ServerRegistration::<Cipher>::deserialize(
+            server_registration_bytes,
+          ) {
+            Ok(server_registration) => Ok(Some(server_registration)),
+            Err(e) => {
+              error!(
+                "Failed to deserialize ServerRegistration struct for user {}: {}",
+                user_id, e
+              );
+              Err(Error::Pake(e))
+            }
+          }
+        } else {
+          error!("No registration data found for registered user {}", user_id);
+          Err(Error::MissingAttribute)
+        }
+      }
+      Ok(_) => {
+        info!(
+          "No item found for user {} in PAKE registration table",
+          user_id
+        );
+        Ok(None)
+      }
+      Err(e) => {
+        error!(
+          "DynamoDB client failed to get registration data for user {}: {}",
+          user_id, e
+        );
+        Err(Error::Rusoto(e))
+      }
+    }
+  }
+}
+
+#[derive(
+  Debug, derive_more::Display, derive_more::From, derive_more::Error,
+)]
+pub enum Error {
+  #[display(...)]
+  Rusoto(RusotoError<GetItemError>),
+  #[display(...)]
+  Pake(ProtocolError),
+  #[display(...)]
+  MissingAttribute,
 }
