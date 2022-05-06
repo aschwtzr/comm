80428b26d5ae2a62f6793fbe420c01dec792c99d
marcinwasowicz <marcinkwasowicz@gmail.com>
2022-05-06 11:48:07 +0200

    Implement translation from raw message string into SQLite database entities
    Summary: Implements in C++ functions that are implemented in message-ops-utils.js
    
    Test Plan: No testing plan. Future diffs will introduce code that was used on iOS and Andrdoid to parse raw message strings and store them in database.
    
    Reviewers: palys-swm, karol-bisztyga
    
    Subscribers:

---
+++ native/cpp/CommonCpp/NativeModules/MessageOperationsUtilities/MessageOperationsUtilities.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "../DatabaseManagers/DatabaseManager.h"
+#include "../DatabaseManagers/entities/Media.h"
+#include "../DatabaseManagers/entities/Message.h"
+
+#include <folly/dynamic.h>
+#include <string>
+#include <vector>
+
+namespace comm {
+class MessageOperationsUtilities {
+public:
+  static std::pair<Message, std::vector<Media>>
+  translateRawMessageInfoToClientDBMessageInfo(
+      const folly::dynamic &rawMessageInfo);
+  static Media translateMediaToClientDBMediaInfo(
+      const folly::dynamic &rawMediaInfo,
+      const std::string &container,
+      const std::string &thread);
+  static std::vector<std::pair<Message, std::vector<Media>>>
+  translateStringToClientDBMessageInfo(std::string rawMessageInfoString);
+  static void storeNotification(std::string rawMessageInfoString);
+};
+} // namespace comm
