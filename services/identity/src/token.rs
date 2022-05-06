a79abc2a9188c19fe6821889eba15bdefee7ce19
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2022-05-06 14:51:06 -0400

    [Identity] Token constructor
    Summary:
    new function to generate a Token. we use the rand crate to generate a random string for the token, and chrono to get the creation time. I used len 512 for the string because that's what OAuth uses for access tokens.
    
    @jimpo, is this the best way to generate a random String in Rust?
    
    Depends on D3957
    
    Test Plan: Called constructor and verified the contents of the Token
    
    Reviewers: jimpo, karol-bisztyga, palys-swm
    
    Subscribers: ashoat, Adrian, yayabosh, jimpo, atul
    
    Differential Revision: https://phabricator.ashoat.com/D3959

--- services/identity/src/token.rs
+++ services/identity/src/token.rs
@@ -1,4 +1,8 @@
 use chrono::{DateTime, Utc};
+use rand::{
+  distributions::{Alphanumeric, DistString},
+  CryptoRng, Rng,
+};
 
 pub enum AuthType {
   Password,
@@ -10,4 +14,15 @@ pub struct AccessToken {
   pub created: Option<DateTime<Utc>>,
   pub auth_type: Option<AuthType>,
   pub valid: Option<bool>,
+}
+
+impl AccessToken {
+  pub fn new(auth_type: AuthType, rng: &mut (impl Rng + CryptoRng)) -> Self {
+    AccessToken {
+      token: Some(Alphanumeric.sample_string(rng, 512)),
+      created: Some(Utc::now()),
+      auth_type: Some(auth_type),
+      valid: Some(true),
+    }
+  }
 }
