bf16b4ad86da6dfebd0d344cbd9eaf5c53620159
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-05-12 14:54:12 +0200

    [services] Backup - Implement generating holder

--- services/backup/src/Tools.h
+++ services/backup/src/Tools.h
@@ -7,5 +7,14 @@ namespace network {
 
 std::string generateRandomString(std::size_t length = 20);
 
+std::string generateHolder(
+    const std::string &backupID,
+    const std::string &resourceID,
+    const std::string &blobHash);
+
+std::string generateHolder(
+    const std::string &backupID,
+    const std::string &blobHash);
+
 } // namespace network
 } // namespace comm
