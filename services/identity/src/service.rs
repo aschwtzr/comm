b534496061b614e663a9eef2e8fc34f58c2c5883
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2022-05-27 14:18:36 -0400

    [services][identity] helper function to start pake login
    Summary:
    Use pake_credential_request bytes to start PAKE login process. Return error if registration data is not found in DDB. If successful, return a ServerLoginStartResult that contains the message to return to the client.
    
    Depends on D4146
    
    Test Plan: will test this with the subsequent diff
    
    Reviewers: jimpo, karol-bisztyga, palys-swm
    
    Subscribers:

--- services/identity/src/service.rs
+++ services/identity/src/service.rs
@@ -1,7 +1,8 @@
 use futures_core::Stream;
 use opaque_ke::{
-  errors::ProtocolError, keypair::Key,
+  errors::ProtocolError, keypair::Key, CredentialRequest,
   RegistrationRequest as PakeRegistrationRequest, RegistrationUpload,
+  ServerLogin, ServerLoginStartParameters, ServerLoginStartResult,
   ServerRegistration,
 };
 use rand::{CryptoRng, Rng};
@@ -109,6 +110,29 @@ impl MyIdentityService {
       .map_err(Error::Database)?;
     Ok(())
   }
+
+  async fn pake_login_start(
+    &self,
+    user_id: String,
+    pake_credential_request: &Vec<u8>,
+    rng: &mut (impl Rng + CryptoRng),
+  ) -> Result<ServerLoginStartResult<Cipher>, Error> {
+    let server_registration =
+      self.client.get_pake_registration(user_id).await?;
+    let pake_credential_request =
+      CredentialRequest::deserialize(pake_credential_request)?;
+    match server_registration {
+      None => Err(Error::MissingRegistration),
+      Some(registration) => ServerLogin::start(
+        rng,
+        registration,
+        &self.config.server_secret_key,
+        pake_credential_request,
+        ServerLoginStartParameters::default(),
+      )
+      .map_err(Error::Pake),
+    }
+  }
 }
 
 #[derive(
@@ -121,4 +145,6 @@ pub enum Error {
   Channel(SendError<Result<RegistrationResponse, Status>>),
   #[display(...)]
   Database(DatabaseError),
+  #[display(...)]
+  MissingRegistration,
 }
