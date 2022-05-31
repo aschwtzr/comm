b6ed33192e9553d1991fa1ae1a15495cbc6bdee5
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-05-31 15:45:40 +0200

    [DRAFT] [services] Tests - Add main
    Summary: todo
    
    Test Plan: todo
    
    Subscribers: ashoat, palys-swm, Adrian, atul, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D4167

--- services/commtest/build.rs
+++ services/commtest/build.rs
@@ -4,10 +4,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
   let target_service = env::var("COMM_TEST_TARGET")
     .map_err(|err| -> String { format!("invalid test target: {}", err) })?;
   // use this when using docker
-  // tonic_build::compile_protos(format!("protos/{}.proto", target_service))?;
-  tonic_build::compile_protos(format!(
-    "../../native/cpp/CommonCpp/grpc/protos/{}.proto",
-    target_service
-  ))?;
+  tonic_build::compile_protos(format!("protos/{}.proto", target_service))?;
+  // use this for building on the host machine
+  // tonic_build::compile_protos(format!(
+  //   "../../native/cpp/CommonCpp/grpc/protos/{}.proto",
+  //   target_service
+  // ))?;
   Ok(())
 }
