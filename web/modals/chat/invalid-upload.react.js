e7676d8bc90d2626d01c46de7e3ebe124389075a
Atul Madhugiri <atulm@berkeley.edu>
2022-06-03 01:11:48 -0400

    [web] Turn `InvalidUploadModal` into a functional component
    Summary:
    Was looking at this as part of the work to create a common `Alert`/`Prompt` component and noticed it might as well be a functional component... so made that change.
    
    Test Plan:
    Dragged a random non-image file into the chat area and the `InvalidUploadModal` component was displayed as expected:
    
    Reviewers:
    palys-swm, def-au1t
    
    Subscribers:

--- web/modals/chat/invalid-upload.react.js
+++ web/modals/chat/invalid-upload.react.js
@@ -7,33 +7,23 @@ import { useModalContext } from '../../modals/modal-provider.react';
 import Modal from '../modal.react';
 import css from './invalid-upload.css';
 
-type Props = {
-  +popModal: () => void,
-};
-class InvalidUploadModal extends React.PureComponent<Props> {
-  render(): React.Node {
-    return (
-      <Modal name="Invalid upload" onClose={this.props.popModal}>
-        <div className={css.modal_body}>
-          <p>We don&apos;t support that file type yet :(</p>
-          <Button
-            onClick={this.props.popModal}
-            type="submit"
-            variant="primary"
-            className={css.ok_button}
-          >
-            OK
-          </Button>
-        </div>
-      </Modal>
-    );
-  }
-}
-
-function ConnectedInvalidUploadModal(): React.Node {
+function InvalidUploadModal(): React.Node {
   const modalContext = useModalContext();
-
-  return <InvalidUploadModal popModal={modalContext.popModal} />;
+  return (
+    <Modal name="Invalid upload" onClose={modalContext.popModal}>
+      <div className={css.modal_body}>
+        <p>We don&apos;t support that file type yet :(</p>
+        <Button
+          onClick={modalContext.popModal}
+          type="submit"
+          variant="primary"
+          className={css.ok_button}
+        >
+          OK
+        </Button>
+      </div>
+    </Modal>
+  );
 }
 
-export default ConnectedInvalidUploadModal;
+export default InvalidUploadModal;
