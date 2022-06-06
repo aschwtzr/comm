30374f5d224d0e39ec45508ab2ff2b2f40cd0964
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-06-06 13:00:35 +0200

    [services] Tests - Add main

--- services/commtest/src/main.rs
+++ services/commtest/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-  println!("hello");
+use std::env;
+
+pub mod proto {
+  tonic::include_proto!("backup");
+}
+
+pub mod backup_utils;
+mod create_new_backup;
+mod send_log;
+
+use backup_utils::BackupServiceClient;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let target_service = env::var("COMM_TEST_TARGET")?;
+  println!("testing service {}", target_service);
+
+  let mut client = BackupServiceClient::connect("http://[::0]:50052").await?;
+
+  let user_id = "user0000".to_string();
+  let device_id = "device0000".to_string();
+
+  let backup_id = create_new_backup::run(&mut client, &user_id, &device_id)
+    .await
+    .map_err(|err| -> String { format!("create new backup error: {}", err) })?;
+  println!("backup id in main: {}", backup_id);
+  send_log::run(&mut client, &user_id, &backup_id)
+    .await
+    .map_err(|err| -> String { format!("send log: {}", err) })?;
+
+  println!("tested");
+  Ok(())
 }
