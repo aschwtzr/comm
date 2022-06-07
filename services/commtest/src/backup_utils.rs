c43e3c5cf8b0a96bd253910cbe5279295d709562
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-06-07 13:32:52 +0200

    [services] Tests - Backup Utils - Add BackupData struct

--- services/commtest/src/backup_utils.rs
+++ services/commtest/src/backup_utils.rs
@@ -3,3 +3,11 @@ pub mod proto {
 }
 
 pub use proto::backup_service_client::BackupServiceClient;
+
+pub struct BackupData {
+  pub user_id: String,
+  pub device_id: String,
+  pub backup_id: String,
+  pub backup_chunk_sizes: Vec<usize>,
+  pub logs_sizes: Vec<Vec<usize>>,
+}
