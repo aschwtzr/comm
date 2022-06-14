d477d4047a3bdaf8004be36bf5901a411ec4823a
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2022-06-14 14:17:15 +0200

    [services][identity] wallet login
    Summary:
    Full implementation of wallet login. Steps:
    1. Parse SIWE message and signature from request
    2. Verify message using siwe-rs crate
    3. If verification is successful, generate a token, store it in the token table in DynamoDB, and then return it to the caller
    
    Note that PAKE login is not covered here. It will be in a separate diff.
    
    Depends on D4286
    
    Test Plan: Successfully signed in with a metamask wallet
    
    Reviewers: jimpo, karol-bisztyga, palys-swm
    
    Subscribers: ashoat, Adrian, atul, yayabosh
    
    Differential Revision: https://phab.comm.dev/D4285

--- services/identity/src/service.rs
+++ services/identity/src/service.rs
@@ -1,12 +1,18 @@
+use chrono::Utc;
 use constant_time_eq::constant_time_eq;
 use futures_core::Stream;
+use rand::rngs::OsRng;
 use rusoto_core::RusotoError;
-use rusoto_dynamodb::GetItemError;
+use rusoto_dynamodb::{GetItemError, PutItemError};
+use siwe::Message;
 use std::pin::Pin;
+use tokio::sync::mpsc;
+use tokio_stream::{wrappers::ReceiverStream, StreamExt};
 use tonic::{Request, Response, Status};
 use tracing::{error, info, instrument};
 
 use crate::database::DatabaseClient;
+use crate::token::{AccessToken, AuthType};
 use crate::{config::Config, database::Error};
 
 pub use proto::identity_service_server::IdentityServiceServer;
@@ -14,6 +20,13 @@ use proto::{
   identity_service_server::IdentityService, LoginRequest, LoginResponse,
   RegistrationRequest, RegistrationResponse, VerifyUserTokenRequest,
   VerifyUserTokenResponse,
+};
+
+use self::proto::login_response::Data;
+use self::proto::WalletLoginResponse;
+use self::proto::{
+  login_request::Data::PakeLoginRequest,
+  login_request::Data::WalletLoginRequest,
 };
 
 mod proto {
@@ -45,12 +58,121 @@ impl IdentityService for MyIdentityService {
   type LoginUserStream =
     Pin<Box<dyn Stream<Item = Result<LoginResponse, Status>> + Send + 'static>>;
 
+  #[instrument(skip(self))]
   async fn login_user(
     &self,
     request: Request<tonic::Streaming<LoginRequest>>,
   ) -> Result<Response<Self::LoginUserStream>, Status> {
-    println!("Got a login request: {:?}", request);
-    unimplemented!()
+    let mut in_stream = request.into_inner();
+    let (tx, rx) = mpsc::channel(1);
+    let client = self.client.clone();
+    tokio::spawn(async move {
+      while let Some(message) = in_stream.next().await {
+        if let Ok(login_request) = message {
+          if let Some(data) = login_request.data {
+            match data {
+              WalletLoginRequest(req) => {
+                let siwe_message: Message = match req.message.parse() {
+                  Ok(m) => m,
+                  Err(e) => {
+                    error!("Failed to parse SIWE message: {}", e);
+                    if let Err(e) = tx
+                      .send(Err(Status::invalid_argument("invalid message")))
+                      .await
+                    {
+                      error!("Response was dropped: {}", e);
+                    }
+                    break;
+                  }
+                };
+                if let Err(e) = siwe_message.verify(
+                  match req.signature.try_into() {
+                    Ok(s) => s,
+                    Err(e) => {
+                      error!("Conversion to SIWE signature failed: {:?}", e);
+                      break;
+                    }
+                  },
+                  None,
+                  None,
+                  Some(&Utc::now()),
+                ) {
+                  error!("Signature verification failed for user {} on device {}: {}", req.user_id, req.device_id, e);
+                  if let Err(e) = tx
+                    .send(Err(Status::unauthenticated(
+                      "message not authenticated",
+                    )))
+                    .await
+                  {
+                    error!("Response was dropped: {}", e);
+                  }
+                  break;
+                } else {
+                  let token = AccessToken::new(
+                    req.user_id,
+                    req.device_id,
+                    AuthType::Wallet,
+                    &mut OsRng,
+                  );
+                  match client.put_token(token.clone()).await {
+                    Ok(_) => {
+                      if let Err(e) = tx
+                        .send(Ok(LoginResponse {
+                          data: Some(Data::WalletLoginResponse(
+                            WalletLoginResponse { token: token.token },
+                          )),
+                        }))
+                        .await
+                      {
+                        error!("Response was dropped: {}", e);
+                      }
+                      break;
+                    }
+                    Err(Error::RusotoPut(RusotoError::Service(
+                      PutItemError::ResourceNotFound(_),
+                    )))
+                    | Err(Error::RusotoPut(RusotoError::Credentials(_))) => {
+                      if let Err(e) = tx
+                        .send(Err(Status::failed_precondition(
+                          "internal error",
+                        )))
+                        .await
+                      {
+                        error!("Response was dropped: {}", e);
+                      }
+                      break;
+                    }
+                    Err(Error::RusotoPut(_)) => {
+                      if let Err(e) =
+                        tx.send(Err(Status::unavailable("please retry"))).await
+                      {
+                        error!("Response was dropped: {}", e);
+                      }
+                      break;
+                    }
+                    Err(e) => {
+                      error!("Encountered an unexpected error: {}", e);
+                      if let Err(e) = tx
+                        .send(Err(Status::failed_precondition(
+                          "unexpected error",
+                        )))
+                        .await
+                      {
+                        error!("Response was dropped: {}", e);
+                      }
+                      break;
+                    }
+                  }
+                }
+              }
+              PakeLoginRequest(_) => unimplemented!(),
+            }
+          }
+        }
+      }
+    });
+    let out_stream = ReceiverStream::new(rx);
+    Ok(Response::new(Box::pin(out_stream) as Self::LoginUserStream))
   }
 
   #[instrument(skip(self))]
