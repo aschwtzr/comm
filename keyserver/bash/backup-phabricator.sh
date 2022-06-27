2926b63d66df9941d26dbc927bf4f7f5278030a6
Abosh Upadhyaya <aboshu@gmail.com>
2022-06-27 13:20:55 -0400

    [keyserver] Clean up `backup-phabricator.sh` with style fixes
    Summary: Relevant Linear task with full context about `keyserver` script cleanup [[ https://linear.app/comm/issue/ENG-1295/clean-up-shell-scripts-in-keyserver | here ]]. Added quotes for preventing globbing, removed `grep` command (and merged with `awk`).
    
    Test Plan:
    [[ https://www.shellcheck.net/ | ShellCheck ]], close reading. Talked about the Test Plan in more detail in the Linear issue, since I did not actually re-run the script. However, since this mainly modifies style, it may not be necessary.
    
    Also, we should plan on adding [[ https://linear.app/comm/issue/ENG-1295#comment-7de88d69 | ShellCheck to the CI ]] as @atul suggested.
    
    Reviewers: atul
    
    Subscribers: ashoat, palys-swm, Adrian, atul
    
    Differential Revision: https://phab.comm.dev/D4365

--- keyserver/bash/backup-phabricator.sh
+++ keyserver/bash/backup-phabricator.sh
@@ -10,13 +10,13 @@ PHABRICATOR_PATH=/var/www/phacility/phabricator
 BACKUP_PATH=/mnt/backup
 
 # The user that will be owning the backup files
-BACKUP_USER=comm
+BACKUP_USER="comm"
 
 # The maximum amount of space to spend on Phabricator backups
 MAX_DISK_USAGE_KB=204800 # 200 MiB
 
 set -e
-[[ `whoami` = root ]] || exec sudo su -c "$0"
+[[ $(whoami) = root ]] || exec sudo su -c "$0"
 
 cd "$PHABRICATOR_PATH"
 
@@ -42,14 +42,14 @@ while [[ $RETRIES -ge 0 ]]; do
   ((RETRIES--))
 done
 
-chown $BACKUP_USER:$(id -gn $BACKUP_USER) "$OUTPUT_FILE" || true
+chown $BACKUP_USER:"$(id -gn $BACKUP_USER)" "$OUTPUT_FILE" || true
 
 while true; do
-  TOTAL_USAGE=$(sudo du -cs "$BACKUP_PATH"/phabricator.*.sql.gz | grep total | awk '{ print $1 }')
+  TOTAL_USAGE=$(sudo du -cs "$BACKUP_PATH"/phabricator.*.sql.gz | awk '/total/ { print $1 }')
   if [[ $TOTAL_USAGE -le $MAX_DISK_USAGE_KB ]]; then
     break
   fi
-  BACKUP_COUNT=$(ls -hla "$BACKUP_PATH"/phabricator.*.sql.gz | wc -l)
+  BACKUP_COUNT=$(find "$BACKUP_PATH" -maxdepth 1 -name 'phabricator.*.sql.gz' -type f | wc -l)
   if [[ $BACKUP_COUNT -lt 2 ]]; then
     break
   fi
