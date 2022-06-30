681273a178bc75b204b348f98f9a5410a8ce91f7
Karol Bisztyga <karol.bisztyga@swmansion.com>
2022-06-30 16:08:14 +0200

    [services] Tests - run cargo fmt
    Summary: Depends on D4379
    Something's wrong, `cargo fmt` should be fired up in git pre commit hooks, this should be fixed.
    
    Test Plan: -
    
    Reviewers:
    
    Subscribers:

--- services/commtest/tests/blob_test.rs
+++ services/commtest/tests/blob_test.rs
@@ -17,24 +17,38 @@ use tools::Error;
 
 #[tokio::test]
 async fn blob_test() -> Result<(), Error> {
-  let port = env::var("COMM_SERVICES_PORT_BLOB").expect("port env var expected but not received");
-  let mut client = BlobServiceClient::connect(format!("http://localhost:{}", port)).await?;
+  let port = env::var("COMM_SERVICES_PORT_BLOB")
+    .expect("port env var expected but not received");
+  let mut client =
+    BlobServiceClient::connect(format!("http://localhost:{}", port)).await?;
 
   let blob_data = vec![
     BlobData {
       holder: "test_holder001".to_string(),
       hash: "test_hash001".to_string(),
-      chunks_sizes: vec![ByteSize::b(100).as_u64() as usize, ByteSize::b(100).as_u64() as usize, ByteSize::b(100).as_u64() as usize],
+      chunks_sizes: vec![
+        ByteSize::b(100).as_u64() as usize,
+        ByteSize::b(100).as_u64() as usize,
+        ByteSize::b(100).as_u64() as usize,
+      ],
     },
     BlobData {
       holder: "test_holder002".to_string(),
       hash: "test_hash002".to_string(),
-      chunks_sizes: vec![tools::get_grpc_chunk_size_limit(), tools::get_grpc_chunk_size_limit(), ByteSize::b(10).as_u64() as usize],
+      chunks_sizes: vec![
+        tools::get_grpc_chunk_size_limit(),
+        tools::get_grpc_chunk_size_limit(),
+        ByteSize::b(10).as_u64() as usize,
+      ],
     },
     BlobData {
       holder: "test_holder003".to_string(),
       hash: "test_hash003".to_string(),
-      chunks_sizes: vec![tools::get_grpc_chunk_size_limit(), ByteSize::b(100).as_u64() as usize, tools::get_grpc_chunk_size_limit()],
+      chunks_sizes: vec![
+        tools::get_grpc_chunk_size_limit(),
+        ByteSize::b(100).as_u64() as usize,
+        tools::get_grpc_chunk_size_limit(),
+      ],
     },
   ];
 
@@ -50,15 +64,16 @@ async fn blob_test() -> Result<(), Error> {
     assert_eq!(
       expected_data_size, received_data_size,
       "invalid size of data for index {}, expected {}, got {}",
-      i,
-      expected_data_size,
-      received_data_size
+      i, expected_data_size, received_data_size
     );
   }
 
   for item in &blob_data {
     remove::run(&mut client, &item).await?;
-    assert!(get::run(&mut client, &item).await.is_err(), "item should no longer be available");
+    assert!(
+      get::run(&mut client, &item).await.is_err(),
+      "item should no longer be available"
+    );
   }
 
   Ok(())
