e70be6452f05d849b4cf5d5ff13400331fc2b38e
Atul Madhugiri <atulm@berkeley.edu>
2022-07-05 09:23:25 -0400

    [keyserver] Use SMTP (via Postmark) instead of `sendmail` for email subscriptions
    Summary:
    The `SubscriptionForm` on the landing page broke when we moved the keyserver to EC2 because the image didn't include `sendmail`. After installing `sendmail` it appeared that things were working and the requests were succeeding, but the emails didn't end up getting delivered reliably. There's additional configuration that's necessary to get the `sendmail` approach working reliably/consistently.
    
    Instead, in this diff we move from the `sendmail` transport to using SMTP via nodemailer with Postmark as the "host."
    
    These are some resources I referenced to possibly save reviewer some googling:
    - https://postmarkapp.com/developer/user-guide/send-email-with-smtp
    - https://www.mailgun.com/blog/email/which-smtp-port-understanding-ports-25-465-587/
    - https://sendgrid.com/blog/whats-the-difference-between-ports-465-and-587/
    
    Test Plan:
    1. Change the `to` field in `sendmail.sendMail` to my Comm email account
    2. Navigate to `localhost/commlanding/`
    3. Put a valid email in the `SubscriptionForm` input
    4. Click "Subscribe for updates"
    5. Observe that the email shows up in Postmark
    6. Ensure that the email showed up in my inbox
    
    Reviewers: def-au1t, palys-swm, yayabosh, ashoat
    
    Subscribers: Adrian
    
    Differential Revision: https://phab.comm.dev/D4291

--- keyserver/src/emails/sendmail.js
+++ keyserver/src/emails/sendmail.js
@@ -1,6 +1,11 @@
 // @flow
 
+import invariant from 'invariant';
 import nodemailer from 'nodemailer';
+
+import { isDev } from 'lib/utils/dev-utils';
+
+import { importJSON } from '../utils/import-json.js';
 
 type MailInfo = {
   +from: string,
@@ -14,6 +19,31 @@ type Transport = {
   ...
 };
 
-const sendmail: Transport = nodemailer.createTransport({ sendmail: true });
+type PostmarkConfig = {
+  +apiToken: string,
+};
 
-export default sendmail;
+async function getSendmail(): Promise<Transport> {
+  const postmark: ?PostmarkConfig = await importJSON({
+    folder: 'facts',
+    name: 'postmark',
+  });
+
+  if (isDev && !postmark) {
+    return nodemailer.createTransport({ sendmail: true });
+  }
+
+  invariant(postmark, 'Postmark config missing');
+  return nodemailer.createTransport({
+    host: 'smtp.postmarkapp.com',
+    port: 587,
+    secure: false,
+    auth: {
+      user: postmark.apiToken,
+      pass: postmark.apiToken,
+    },
+    requireTLS: true,
+  });
+}
+
+export default getSendmail;
