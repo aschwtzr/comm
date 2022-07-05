a8adb357a2eb709edc834f9b8efd7c3b6ff881a9
Jacek Nitychoruk <jacek.nitychoruk@swmansion.com>
2022-07-05 16:57:58 +0200

    [web] Introduce pending thread creation in `navReducer` if it is missing
    Summary:
    The diff introduces mechanism for setting pendingThread in navInfo, so it match `activeChatThreadID` value.
    The reason is to load pending thread correctly in navigation after page refresh - when we know only pending thread ID from URL.
    The solution doesn't work for sidebars right now. Creating pending sidebar would require addidtional data (like parent thread info) that should be found efficiently using only `sourceMessageID`, and it could be introduced in the future.
    
    Test Plan: Run web app and using search find user, that has no chat with our user. Clicking that user will create pending personal chat. After refreshing the page, the pending chat should be loaded automatically and user is not redirected to first chat from list.
    
    Reviewers: palys-swm, atul
    
    Subscribers: ashoat, Adrian, yayabosh
    
    Differential Revision: https://phab.comm.dev/D4455

--- web/redux/nav-reducer.js
+++ web/redux/nav-reducer.js
@@ -1,8 +1,13 @@
 // @flow
 
 import { locallyUniqueToRealizedThreadIDsSelector } from 'lib/selectors/thread-selectors';
-import { threadIsPending } from 'lib/shared/thread-utils';
-import type { RawThreadInfo } from 'lib/types/thread-types';
+import {
+  createPendingThread,
+  parseLocallyUniqueThreadID,
+  threadIsPending,
+} from 'lib/shared/thread-utils';
+import { type RawThreadInfo, threadTypes } from 'lib/types/thread-types';
+import type { UserInfos } from 'lib/types/user-types';
 
 import type { Action } from '../redux/redux-setup';
 import { type NavInfo, updateNavInfoActionType } from '../types/nav-types';
@@ -11,6 +16,8 @@ export default function reduceNavInfo(
   oldState: NavInfo,
   action: Action,
   newThreadInfos: { +[id: string]: RawThreadInfo },
+  userID: ?string,
+  userInfos: UserInfos,
 ): NavInfo {
   let state = oldState;
   if (action.type === updateNavInfoActionType) {
@@ -20,7 +27,7 @@ export default function reduceNavInfo(
     };
   }
 
-  const { activeChatThreadID } = state;
+  const { activeChatThreadID, pendingThread } = state;
   if (activeChatThreadID) {
     const locallyUniqueToRealizedThreadIDs = locallyUniqueToRealizedThreadIDsSelector(
       newThreadInfos,
@@ -33,11 +40,40 @@ export default function reduceNavInfo(
         ...state,
         activeChatThreadID: realizedThreadID,
       };
+    } else if (
+      threadIsPending(activeChatThreadID) &&
+      pendingThread?.id !== activeChatThreadID
+    ) {
+      const pendingThreadData = parseLocallyUniqueThreadID(activeChatThreadID);
+      // We do not create a pending sidebar yet, because it would require
+      // an efficient way of finding parent thread basing on source message ID
+      if (
+        pendingThreadData &&
+        pendingThreadData.threadType !== threadTypes.SIDEBAR &&
+        userID
+      ) {
+        const members = pendingThreadData.memberIDs
+          .map(id => userInfos[id])
+          .filter(Boolean);
+        const newPendingThread = createPendingThread({
+          viewerID: userID,
+          threadType: pendingThreadData.threadType,
+          members,
+        });
+        state = {
+          ...state,
+          activeChatThreadID: newPendingThread.id,
+          pendingThread: newPendingThread,
+        };
+      }
     }
   }
 
   if (state.pendingThread && !threadIsPending(state.activeChatThreadID)) {
-    const { pendingThread, ...stateWithoutPendingThread } = state;
+    const {
+      pendingThread: currentPendingThread,
+      ...stateWithoutPendingThread
+    } = state;
     state = stateWithoutPendingThread;
   }
 
