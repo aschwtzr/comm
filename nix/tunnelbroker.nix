a29f8e1322422138b7704402449e1a3a3c9c1864
Jonathan Ringer <jonringer117@gmail.com>
2022-07-09 00:14:24 -0700

    [Nix] Package tunnelbroker
    Summary: Package tunnelbroker through nix
    Also export a docker image version of the package.
    
    Test Plan: `nix build .#comm-tunnelbroker`
    
    Reviewers: varun
    
    Subscribers:

---
+++ nix/tunnelbroker.nix
@@ -0,0 +1,77 @@
+{ lib
+, stdenv
+, amqp-cpp
+, aws-sdk-cpp
+, boost
+, cmake
+, cryptopp
+, double-conversion
+, folly
+, fmt
+, glog
+, grpc
+, libuv
+, ninja
+, openssl
+, pkg-config
+, protobuf_3_15_cmake
+, sqlite
+
+# docker image
+, dockerTools
+}:
+
+let
+  self = stdenv.mkDerivation rec {
+    name = "comm-tunnelbroker";
+
+    src = ../.;
+
+    prePatch = ''
+      cd services/tunnelbroker
+    '';
+
+    nativeBuildInputs = [
+      cmake
+      ninja
+      pkg-config
+    ];
+
+    buildInputs = [
+      amqp-cpp
+      aws-sdk-cpp
+      boost
+      cryptopp
+      double-conversion
+      fmt
+      folly
+      glog
+      grpc
+      libuv
+      openssl
+      protobuf_3_15_cmake
+      sqlite
+    ];
+
+    # TODO: Fix upstream logic to allow for aws-sdk-cpp to work with headers
+    #  and libs being located in different directories
+    cmakeFlags = [
+      "-DAWSSDK_ROOT_DIR=${lib.getDev aws-sdk-cpp}"
+      "-DAWSSDK_CORE_HEADER_FILE=${lib.getDev aws-sdk-cpp}/include/aws/core/Aws.h"
+    ];
+
+    # export a dockerImage along side of the service
+    passthru.dockerImage = dockerTools.buildImage {
+      name = "tunnelbroker";
+      tag = "latest";
+
+      contents = [ self ];
+
+      config = {
+        Cmd = [ "/bin/tunnelbroker" ];
+      };
+    };
+  };
+in
+  self
+
